782ea0558c10aa962f06df2699f00f9fb0846cbf
NadeGolubeva <colombe466@gmail.com>
2023-03-01 19:19:27 +0200

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,12 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector("iframe");
 
 const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
 player.on("timeupdate", throttle(onPlay, 1000));
 
 function onPlay(evt) {
-  //   console.log(e.seconds);
-  localStorage.setItem("videoplayer-current-time", evt.seconds);
+  localStorage.setItem(STORAGE_KEY, evt.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,46 +8,24 @@ const STOREGY_KEY = "feedback-form-state";
 form.addEventListener("input", throttle(onInput, 500));
 
 function onInput(evt) {
-  // console.log(email.value);
-  // console.log(message.value);
-
   const datas = { email: email.value, message: message.value };
-
-  // console.log(datas);
   localStorage.setItem(STOREGY_KEY, JSON.stringify(datas));
 }
-// - При загрузке страницы проверяй состояние хранилища,
-//   и если там есть сохраненные данные, заполняй ими поля
-// формы.В противном случае поля должны быть пустыми.
-// - При сабмите формы очищай хранилище и поля формы,
-//   а также выводи объект с полями email, message и
-//   текущими их значениями в консоль.
-
-function onLoad() {
-  const savedDatas = localStorage.getItem(STOREGY_KEY);
-  if (savedDatas === "") {
-    return;
-    // message.value = savedDatas;
-  }
-
-  console.log(savedDatas);
-
-  const objData = JSON.parse(savedDatas);
-  console.log(objData);
-  email.value = objData.email;
-  message.textContent = objData.message;
-  console.log(email.textContent);
-  console.log(message.textContent);
-  // objData.email;
-}
-onLoad();
 
 form.addEventListener("submit", onSubmit);
 function onSubmit(evt) {
   evt.preventDefault();
   if (email.value === "" || message.value === "") return;
   console.log({ email: email.value, message: message.value });
-  // localStorage.removeItem(STOREGY_KEY);
 
   form.reset();
+
+  localStorage.removeItem(STOREGY_KEY);
+}
+
+const savedDatas = localStorage.getItem(STOREGY_KEY);
+const objData = JSON.parse(savedDatas);
+if (savedDatas) {
+  email.value = objData.email;
+  message.textContent = objData.message;
 }
